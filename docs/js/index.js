133e92500658b92b48d9d8bcfc0073bf500cc445
th-mong <taehyeong8580@gmail.com>
2025-06-03 10:00:41 +0900

    Add files via upload

---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll(".fade-in");
+
+    function handleScroll() {
+        faders.forEach(el => {
+            const rect = el.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight - 100;
+
+            if (isVisible) {
+                el.classList.add("show");
+            }
+        });
+        }
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+});
